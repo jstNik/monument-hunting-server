65ece27a0e0d3ca0464883747074c0103e3ec15a
nik <niccolo.leoncini@gmail.com>
2024-11-19 16:51:38 +0100

    Setting up database

--- players_riddles/urls.py
+++ players_riddles/urls.py
@@ -5,5 +5,5 @@ from .views import PlayersRiddlesView
 app_name = "players_riddles"
 
 urlpatterns = [
-    path("player/<int:pk>", PlayersRiddlesView.as_view(), name="players_riddles_view")
+    path("player/<int:pk>/", PlayersRiddlesView.as_view(), name="players_riddles_view")
 ]
